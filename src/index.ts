9aa09d9d0699f8a2caaa5289f55f78fb411811f8
Badal <badalsingh120403@gmail.com>
2025-03-30 18:46:43 +0000

    fefat: Create main entry point for Processing service
    - Implements the main startup sequence in src/index.ts.
    - Connects to RabbitMQ on startup.
    - Instantiates the VideoProcessingConsumer with appropriate queue and
    binding key.
    - Starts the consumer, passing the video processing handler function.
    - Includes error handling for startup and graceful shutdown procedures.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,50 @@
-console.log('Hello');
+import { VIDEO_UPLOAD_COMPLETED_ROUTING_KEY } from './config/constants';
+import {
+  closeRabbitMQConnection,
+  connectRabbitMQ,
+} from './config/rabbitmq-client';
+import { VideoProcessingConsumer } from './consumers/VideoProcessingConsumer';
+import { handleVideoUploadEvent } from './handlers/videoProcessingHandler';
+
+const QUEUE_NAME = 'video_processing_queue';
+const BINDING_KEY = VIDEO_UPLOAD_COMPLETED_ROUTING_KEY;
+
+async function main() {
+  console.log('--- Video Processing Service Starting ---');
+  let consumer: VideoProcessingConsumer | null = null;
+
+  try {
+    await connectRabbitMQ();
+    consumer = new VideoProcessingConsumer(QUEUE_NAME, BINDING_KEY);
+    await consumer.start(handleVideoUploadEvent);
+
+    console.log('--- Video Processing Service Started Successfully ---');
+  } catch (error) {
+    console.error('Failed to start Video Processing Service:', error);
+
+    await shutdown();
+    process.exit(1);
+  }
+}
+
+let isShuttingDown = false;
+async function shutdown() {
+  if (isShuttingDown) return;
+
+  isShuttingDown = true;
+  console.log('\n--- Video Processing Service Shutting Down ---');
+
+  await closeRabbitMQConnection();
+  console.log(`RabbitMQ connection closed`);
+  console.log(`Shutdown complete.`);
+}
+process.on('SIGINT', async () => {
+  await shutdown();
+  process.exit(1);
+});
+process.on('SIGTERM', async () => {
+  await shutdown();
+  process.exit(1);
+});
+
+main();
